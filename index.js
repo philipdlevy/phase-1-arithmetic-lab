b2dc6d973ae0d90f5fd361128ff609f01b42b54f
Philip Levy <philipdlevy@gmail.com>
2022-02-04 07:32:49 -0700

    commit tree daa9f0bda887d92f454e99b2c9eac7856999a01f

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+
+
+const random = Math.ceil(Math.random());
+
+let num3 = 22;
+let num4 = 6;
+const mod = num3 % num4;
+
+Math.max(1, 2, 4, 10, 20);
+const max = 20;
